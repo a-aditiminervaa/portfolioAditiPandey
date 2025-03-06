b35461e9ac297e36966e00f57226bb57147d4ed9
ADITI PANDEY <160531006+a-aditiminerva@users.noreply.github.com>
2025-03-06 22:32:15 +0530

    portfolio

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const menuToggle = document.getElementById('menuToggle');
+const menu = document.getElementById('menu');
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+});
+
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
